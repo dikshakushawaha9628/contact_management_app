26a8260dd045fd125fa1a39d056ecd5ebbeccd6d
Diksha Kushawaha <hkushwaha880@gmail.com>
2026-01-03 14:55:32 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(express.json());
 
+app.use(cors({
+  origin: [`${process.env.FRONTEND_URL}`],
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true
+}));
+app.use(express.json());
 // Routes
 app.use('/api/contacts', contactRoutes);
 
@@ -22,7 +26,7 @@ app.get('/', (req, res) => {
 });
 
 // MongoDB connection
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://27017/contact_management';
+const MONGODB_URI = process.env.MONGODB_URI 
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
